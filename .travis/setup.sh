bc7d15620f652fa547f39fab42757b4af60f34a0
josemiguelq <jose.miguelq95@gmail.com>
2020-04-01 10:50:36 -0300

    comments

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "as";
-echo $DRIVER_VERSION;
+
 if [[ ${TRAVIS_PHP_VERSION:0:2} = "5." ]] ; \
     then yes '' | pecl -q install -f mongo-"$DRIVER_VERSION"; \
 fi
@@ -8,6 +7,5 @@ if [[ ${TRAVIS_PHP_VERSION:0:2} = "7." ]] ; \
     then composer config "platform.ext-mongo" "1.6.16" \
     && composer  require alcaeus/mongo-php-adapter="$ADAPTER_VERSION"; \
 fi
-composer config "platform.ext-mongo" "1.6.16" \
-    && composer require "alcaeus/mongo-php-adapter=^1.0.0" \
-    && composer update \
+
+composer update
